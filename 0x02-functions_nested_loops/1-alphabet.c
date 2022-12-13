1f70affb6bd67022990cc5c0ffc84e03eb5d378f
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2022-12-13 12:04:24 -0800

    task0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,18 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	int(a - z);
-	{
-		_putchar("a b c d e f g h i j k l m n o p q r st u v w x y z");
-		void print_alphabet(void);
 
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
 	}
-	return (0);
+
+	putchar('\n');
+
 }
