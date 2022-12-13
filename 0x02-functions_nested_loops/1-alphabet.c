01501ac5c80ff52dd4cc3e11323f8841a36cba87
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2022-12-13 11:28:57 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int(a - z);
+	{
+		_putchar("a b c d e f g h i j k l m n o p q r st u v w x y z");
+		void print_alphabet(void);
+
+	}
+	return (0);
+}
