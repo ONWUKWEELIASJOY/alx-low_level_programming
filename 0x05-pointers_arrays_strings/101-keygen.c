af57d7b71aef178a04a8222cdc608df978514c95
ONWUKWEELIASJOY <ddminaj1@gmail.com>
2022-12-19 14:08:16 -0800

    genpsswd

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,27 +10,27 @@
 
 int main(void)
 {
-	char password[84];
+	char password[74];
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
 
-	while (sum < 2772)
+	while (sum < 3882)
 	{
-		password[index] = 33 + rand() % 94;
+		password[index] = 66 + rand() % 84;
 		sum += password[index++];
 	}
 	password[index] = '\0';
 
-	if (sum != 2772)
+	if (sum != 3882)
 	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-		if ((sum - 2772) % 2 != 0)
+		diff_half1 = (sum - 3882) / 2;
+		diff_half2 = (sum - 3882) / 2;
+		if ((sum - 3882) % 2 != 0)
 			diff_half1++;
 		for (index = 0; password[index]; index++)
 		{
-			if (password[index] >= (33 + diff_half1))
+			if (password[index] >= (66 + diff_half1))
 			{
 				password[index] -= diff_half1;
 				break;
@@ -38,7 +38,7 @@ int main(void)
 		}
 		for (index = 0; password[index]; index++)
 		{
-			if (password[index] >= (33 + diff_half2))
+			if (password[index] >= (66 + diff_half2))
 			{
 				password[index] -= diff_half2;
 				break;
